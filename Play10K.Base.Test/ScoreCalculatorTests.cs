44c5ba5a7bc4b60b1e3b5909413d8a904d65311b
MathiasLorenz <lorenz.mathias@gmail.com>
2020-05-23 16:01:06 +0200

    Tests for ScoreCalculator.

---
+++ Play10K.Base.Test/ScoreCalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Play10K.Base.Test
+{
+    [TestClass]
+    public class ScoreCalculatorTests
+    {
+        [TestMethod]
+        [DataRow(new [] { 1, 3 }, 1000)]
+        [DataRow(new [] { 1, 1 }, 100)]
+        [DataRow(new [] { 5, 1 }, 50)]
+        [DataRow(new [] { 4, 4 }, 800)]
+        [DataRow(new [] { 2, 6 }, 1600)]
+        [DataRow(new [] { 6, 3 }, 600)]
+        [DataRow(new [] { 2, 5 }, 800)]
+        [DataRow(new [] { 1, 6 }, 8000)]
+        public void CalculateScore_Calculate_ReturnsCorrect(int[] hand, int expected)
+        {
+            var diceCollection = new DiceCollection(hand[0], hand[1]);
+            var result = ScoreCalculator.CalculateScore(diceCollection);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        [DataRow(new[] { 1, 2 })]
+        [DataRow(new[] { 0, 1 })]
+        [DataRow(new[] { 7, 3 })]
+        [DataRow(new[] { 2, 7 })]
+        [DataRow(new[] { 5, 0 })]
+        public void CalculateScore_Calculate_ThrowsException(int[] hand)
+        {
+            var diceCollection = new DiceCollection(hand[0], hand[1]);
+            ScoreCalculator.CalculateScore(diceCollection);
+        }
+    }
+}
--- Play10K.Base/ScoreCalculator.cs
+++ Play10K.Base/ScoreCalculator.cs
@@ -8,7 +8,34 @@ namespace Play10K.Base
     {
         public static int CalculateScore(DiceCollection input)
         {
-            return 2;
+            return input.Count switch
+            {
+                1 => input.Value switch
+                {
+                    1 => 100,
+                    5 => 50,
+                    _ => throw new ArgumentException("Cannot calculate score for this DiceCollection"),
+                },
+                3 => ThreeDiceCombinationScores(input.Value),
+                4 => 2*ThreeDiceCombinationScores(input.Value),
+                5 => 4*ThreeDiceCombinationScores(input.Value),
+                6 => 8*ThreeDiceCombinationScores(input.Value),
+                _ => throw new ArgumentException("Cannot calculate score for this DiceCollection"),
+            };
+        }
+
+        private static int ThreeDiceCombinationScores(int value)
+        {
+            return value switch
+            {
+                1 => 1000,
+                2 => 200,
+                3 => 300,
+                4 => 400,
+                5 => 500,
+                6 => 600,
+                _ => throw new ArgumentException("Cannot calculate score for this DiceCollection"),
+            };
         }
     }
 }
