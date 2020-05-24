90e634bb82e412cbd0f3207c83d167a08de426dd
MathiasLorenz <lorenz.mathias@gmail.com>
2020-05-24 13:22:29 +0200

    Tests and fixes of CollectedDice. Some logic will have to change for this to work, coming up!

---
+++ Play10K.Base.Test/CollectedDiceTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Play10K.Base.Test
+{
+    [TestClass]
+    public class CollectedDiceTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 5 }, 50)]
+        [DataRow(new int[] { 1 }, 100)]
+        [DataRow(new int[] { 3, 3, 3 }, 300)]
+        [DataRow(new int[] { 4, 4, 4, 4, 4 }, 1600)]
+        [DataRow(new int[] { 1, 1, 1 }, 1000)]
+        [DataRow(new int[] { 1, 1, 1, 1 }, 2000)]
+        [DataRow(new int[] { 6, 6, 6, 6, 6, 6 }, 4800)]
+        [DataRow(new int[] { 1, 1 }, 200)]
+        [DataRow(new int[] { 5, 5 }, 100)]
+        [DataRow(new int[] { 1, 5 }, 150)]
+        [DataRow(new int[] { 5, 1 }, 150)]
+        [DataRow(new int[] { 1, 1, 5 }, 250)]
+        [DataRow(new int[] { 5, 5, 1 }, 200)]
+        [DataRow(new int[] { 1, 5, 1 }, 250)]
+        [DataRow(new int[] { 1, 5, 1, 1 }, 350)]
+        [DataRow(new int[] { 1, 5, 1, 1, 1 }, 450)]
+        [DataRow(new int[] { 1, 5, 1, 1, 1 }, 450)]
+        [DataRow(new int[] { 1, 5, 1, 1, 1, 1 }, 550)]
+        public void SaveDice_SingleHand_ReturnsTrue(int[] toCollect, int expectedScore)
+        {
+            var collectedDice = new CollectedDice();
+            var result = collectedDice.SaveDice(toCollect);
+            var expectedCollect = true;
+
+            Assert.AreEqual(expectedCollect, result);
+            Assert.AreEqual(expectedScore, collectedDice.Score);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 2 })]
+        [DataRow(new int[] { 3 })]
+        [DataRow(new int[] { 4 })]
+        [DataRow(new int[] { 6 })]
+        [DataRow(new int[] { 3, 3 })]
+        [DataRow(new int[] { 4, 4 })]
+        [DataRow(new int[] { 1, 4 })]
+        [DataRow(new int[] { 5, 6 })]
+        [DataRow(new int[] { 1, 2, 5 })]
+        [DataRow(new int[] { 5, 3, 5 })]
+        [DataRow(new int[] { 3, 3, 5 })]
+        [DataRow(new int[] { 6, 6, 6, 2 })]
+        [DataRow(new int[] { 1, 1, 6, 5 })]
+        [DataRow(new int[] { 1, 1, 5, 5, 5 })]
+        [DataRow(new int[] { 1, 1, 5, 3, 2, 1 })]
+        public void SaveDice_SingleHand_ReturnsFalse(int[] toCollect)
+        {
+            var collectedDice = new CollectedDice();
+            var result = collectedDice.SaveDice(toCollect);
+            var expected = false;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void SaveDice_MultipleHands()
+        {
+            var collectedDice = new CollectedDice();
+            var result = collectedDice.SaveDice(new int[] { 3, 3, 3 });
+            var expected = true;
+            Assert.AreEqual(expected, result);
+
+            result = collectedDice.SaveDice(new int[] { 4 });
+            expected = false;
+            Assert.AreEqual(expected, result);
+
+            result = collectedDice.SaveDice(new int[] { 3 });
+            expected = true;
+            Assert.AreEqual(expected, result);
+
+            result = collectedDice.SaveDice(new int[] { 1 });
+            expected = true;
+            Assert.AreEqual(expected, result);
+
+            result = collectedDice.SaveDice(new int[] { 3 });
+            expected = false;
+            Assert.AreEqual(expected, result);
+
+            result = collectedDice.SaveDice(new int[] { 5 });
+            expected = true;
+            Assert.AreEqual(expected, result);
+
+            var score = 750;
+            Assert.AreEqual(score, collectedDice.Score);
+        }
+    }
+}
--- Play10K.Base/CollectedDice.cs
+++ Play10K.Base/CollectedDice.cs
@@ -18,7 +18,7 @@ namespace Play10K.Base
         /// </summary>
         /// <param name="dice"></param>
         // Todo: Unit test
-        public bool SaveDice(List<int> dice)
+        public bool SaveDice(ICollection<int> dice)
         {
             DiceCollection? lastCollected = _collectedDice.LastOrDefault();
             if (dice.Count <= 0 || dice.Count > 6)
@@ -30,7 +30,18 @@ namespace Play10K.Base
                 var die = dice.Single();
                 if (lastCollected?.Count >= 3)
                 {
-                    lastCollected.AddToCount(1);
+                    if (die == lastCollected.Value)
+                    {
+                        lastCollected.AddToCount(1);
+                    }
+                    else if (die == 1 || die == 5)
+                    {
+                        _collectedDice.Add(new DiceCollection(die, 1));
+                    }
+                    else
+                    {
+                        return false;
+                    }
                 }
                 else // => lastCollected?.Count == 1
                 {
@@ -46,23 +57,21 @@ namespace Play10K.Base
             }
             else if (dice.Count == 2)
             {
-                if (dice.Any(x => x != 1 || x != 5))
+                if (dice.Any(x => x != 1 && x != 5))
                 {
                     return false;
                 }
                 else if (dice.All(x => x == 1) || dice.All(x => x == 5))
                 {
-                    if (dice.First() == lastCollected?.Value)
+                    var die = dice.First();
+                    if (die == lastCollected?.Value && lastCollected?.Count >= 3)
                     {
-                        if (lastCollected?.Count >= 3)
-                        {
-                            lastCollected.AddToCount(2);
-                        }
-                        else
-                        {
-                            _collectedDice.Add(new DiceCollection(dice.First(), 1));
-                            _collectedDice.Add(new DiceCollection(dice.Last(), 1));
-                        }
+                        lastCollected.AddToCount(2);
+                    }
+                    else
+                    {
+                        _collectedDice.Add(new DiceCollection(die, 1));
+                        _collectedDice.Add(new DiceCollection(die, 1));
                     }
                 }
                 else // => {1, 5} or {5, 1}
@@ -73,7 +82,21 @@ namespace Play10K.Base
             }
             else // => 3 <= Count <= 6
             {
-                if (dice.AllElementsEqual()) // If all elements are the same
+                if (dice.AllElementsEqual() == false) // If all elements are NOT the same
+                {
+                    if (dice.Any(x => x != 1 && x != 5))
+                    {
+                        return false;
+                    }
+                    else // Consists of only 1 and 5
+                    {
+                        foreach (var die in dice)
+                        {
+                            _collectedDice.Add(new DiceCollection(die, 1));
+                        }
+                    }
+                }
+                else
                 {
                     if (dice.Count == 3)
                     {
